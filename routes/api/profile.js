334cfb0965d07ea905292935de7e72592e3d5116
losk773 <losk773@gmail.com>
2020-05-28 14:32:02 +0300

    added request for delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -200,4 +200,25 @@ router.put('/experience', [
   }
 });
 
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Remove profile experience
+// @access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.user.id});
+    
+    // Get remove index
+    const removeIndex = profile.experience.map(exp => exp.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+    
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
