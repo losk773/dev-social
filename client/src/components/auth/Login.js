cefeac7fba6f7053b072810afa0ae018b9e327cd
losk773 <losk773@gmail.com>
2020-06-02 15:52:31 +0300

    updated login and register components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = ({target}) => setFormData({
+    ...formData, [target.name]: target.value 
+  });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    
+    console.log('SUCCESS');
+  }
   return (
-    <div>
-      Login
-    </div>
+    <React.Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input 
+            type="email" 
+            placeholder="Email Address" 
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </React.Fragment>
   )
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = ({target}) => setFormData({
+    ...formData, [target.name]: target.value 
+  });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log('SUCCESS');
+    }
+  }
   return (
-    <div>
-      Register
-    </div>
+    <React.Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="Name" 
+            name="name" 
+            value={name}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            type="email" 
+            placeholder="Email Address" 
+            name="email"
+            value={email}
+            onChange={onChange}
+          />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small
+          >
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link href="/login">Sign In</Link>
+      </p>
+    </React.Fragment>
   )
 };
