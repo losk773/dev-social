543cf6807b822300fc695c39272117400da460a2
losk773 <losk773@gmail.com>
2020-06-30 14:58:12 +0300

    updated Dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Spinner } from '../layout/Spinner';
+import { Link } from 'react-router-dom';
 
-export const Dashboard = ({getProfile, user, profile}) => {
-
+export const Dashboard = ({getProfile, user: { user }, profile: { loading, profile }}) => {
   useEffect(() => {
-    getProfile();
+    if (!profile) {
+      getProfile();
+    }
   }, []);
 
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+  return loading && profile === null ? <Spinner/> : (
+    <React.Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>{' '}
+        Welcome {user && user.name}
+      </p>
+      {profile !== null ? (
+        <React.Fragment>
+          has
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to='/create-profile' className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
 }
 
 Dashboard.propTypes = {
