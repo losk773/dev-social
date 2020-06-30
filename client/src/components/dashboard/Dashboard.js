5e4fb51d5c6e00994091c30880b6544ade210a17
losk773 <losk773@gmail.com>
2020-06-30 13:07:17 +0300

    created Dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-export const Dashboard = props => {
+export const Dashboard = ({getProfile, user, profile}) => {
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
     <div>
-      
+      Dashboard
     </div>
   )
 }
 
 Dashboard.propTypes = {
-
-}
+  getProfile: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
