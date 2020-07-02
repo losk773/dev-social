1d0508105b249ee6000f985ecb8fa1d7b0bde527
losk773 <losk773@gmail.com>
2020-07-02 12:12:46 +0300

    remove extra parameter from createProfile thunk

--- client/src/containers/CreateProfile.js
+++ client/src/containers/CreateProfile.js
@@ -4,7 +4,7 @@ import { CreateProfile } from '../components/profile-forms/CreateProfile';
 import { createProfile } from '../thunks';
 
 const mapDispatchToProps = (dispatch, { history }) => ({
-  createProfile: (formData, edit) => dispatch(createProfile(formData, history, edit))
+  createProfile: (formData) => dispatch(createProfile(formData, history))
 });
 
 export default withRouter(connect(null, mapDispatchToProps)(CreateProfile));
