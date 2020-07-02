26423a611b9d3b5dba6f8511c0136a38fe9c1e47
losk773 <losk773@gmail.com>
2020-07-02 13:56:42 +0300

    created thunks for add experience and add education

--- client/src/thunks/profile.js
+++ client/src/thunks/profile.js
@@ -3,6 +3,7 @@ import { v4 as uuid } from 'uuid';
 import {
   getProfileSuccess,
   getProfileError,
+  updateProfileSuccess,
   setAlert,
   removeAlert,
 } from '../actions';
@@ -31,6 +32,66 @@ export const createProfile = (formData, history, edit = false) => async dispatch
     if (!edit) {
       history.push('/dashboard');
     }
+
+    setTimeout(() => {
+      dispatch(removeAlert());
+    }, 3000);
+  } catch (error) {
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch(getProfileError({
+      msg: error.response.statusText, 
+      status: error.response.status
+    }));
+
+    setTimeout(() => {
+      dispatch(removeAlert());
+    }, 3000);
+  }
+};
+
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const { data } = await axios.put('/api/profile/experience', formData);
+
+    dispatch(updateProfileSuccess(data));
+    dispatch(setAlert('Experience added', 'success'));
+
+    history.push('/dashboard');
+
+    setTimeout(() => {
+      dispatch(removeAlert());
+    }, 3000);
+  } catch (error) {
+    const errors = error.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch(getProfileError({
+      msg: error.response.statusText, 
+      status: error.response.status
+    }));
+
+    setTimeout(() => {
+      dispatch(removeAlert());
+    }, 3000);
+  }
+};
+
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const { data } = await axios.put('/api/profile/education', formData);
+
+    dispatch(updateProfileSuccess(data));
+    dispatch(setAlert('Education added', 'success'));
+
+    history.push('/dashboard');
 
     setTimeout(() => {
       dispatch(removeAlert());
