228072d3b6bd3616bdd1eac485d546d6f31695e0
losk773 <losk773@gmail.com>
2020-07-07 01:43:14 +0300

    added Profiles and ProfileItem components

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Spinner } from '../../components/layout/Spinner';
+import { ProfileItem } from './ProfileItem';
+
+export const Profiles = ({profile: { profiles, loading }, getProfiles}) => {
+  useEffect(() => {
+    getProfiles();
+  }, []);
+
+  return loading ? <Spinner/> : (
+    <React.Fragment>
+      <h1 className="large text-primary">Developers</h1>
+      <p className="lead">
+        <i className="fab fa-connectdevelop"></i>{' '}
+        Browse and connect with developers
+      </p>
+      <div className="profiles">
+        { profiles.length > 0 ? (
+          profiles.map(profile => (
+            <ProfileItem key={profile._id} profile={profile}/>
+          ))
+        ) : (
+          <h4>No profiles found...</h4>
+        )}
+      </div>
+    </React.Fragment>
+  );
+}
+
+Profiles.propTypes = {
+  getProfiles: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
