bf9298c609ec3e69d88c847059c05b244e75f573
losk773 <losk773@gmail.com>
2020-07-08 01:05:16 +0300

    created ProfileGithub component

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import { Spinner } from '../layout/Spinner';
+
+export const ProfileGithub = ({username, repos, getGithubRepos}) => {
+  useEffect(() => {
+    getGithubRepos(username);
+  }, []);
+
+  return (
+    <div className="profile-github">
+      <h2 className="text-primary my-1">
+        <i className="fab fa-github"></i> Github Repos
+      </h2>
+      {
+        repos.length <= 0 ? <Spinner/> : repos.map(repo => (
+          <React.Fragment>
+            <div key={repo.id} className="repo bg-white p-1 my-1">
+              <div>
+                <h4><a href={repo.html_url} target="_blank" rel="noopener noreferrer">{repo.name}</a></h4>
+                <p>{repo.description}</p>
+              </div>
+              <div>
+                <ul>
+                  <li className="badge badge-primary">Stars: {repo.stargazers_count}</li>
+                  <li className="badge badge-dark">Watchers:{repo.watchers_count}</li>
+                  <li className="badge badge-light">Forks: {repo.forks_count}</li>
+                </ul>
+              </div>
+            </div>
+          </React.Fragment>
+        ))
+      }
+    </div>
+  )
+}
+
+ProfileGithub.propTypes = {
+  username: PropTypes.string.isRequired,
+  getGithubRepos: PropTypes.func.isRequired,
+  repos: PropTypes.array.isRequired,
+}
