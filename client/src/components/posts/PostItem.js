a7f82897c9df1bff9945f290fe465d30508fc222
losk773 <losk773@gmail.com>
2020-07-08 04:08:01 +0300

    added new prop for PostItem component

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -18,8 +18,8 @@ export const PostItem = ({
   addLike,
   removeLike,
   deletePost,
+  showActions = true,
 }) => {
-
   return (
     <div className="post bg-white p-1 my-1">
       <div>
@@ -29,44 +29,51 @@ export const PostItem = ({
         </Link>
       </div>
       <div>
-        <p className="my-1">{text}</p>
+        <Link to={`/posts/${_id}`} className="my-1">{text}</Link>
         <p className="post-date"><Moment format='YYYY/MM/DD'>{date}</Moment></p>
-        <button onClick={() => addLike(_id)} type="button" className="btn btn-light">
-          <i className="fas fa-thumbs-up"></i>{' '}
-          <span>
-            {likes.length > 0 && (
-              <span>{likes.length}</span>
-            )}
-          </span>
-        </button>
-        <button 
-          type="button" 
-          className="btn btn-light"
-          onClick={() => removeLike(_id)}
-          disabled={likes.length === 0 ? 'disabled' : ''}
-        >
-          <i className="fas fa-thumbs-down"></i>
-        </button>
-        <Link to={`/post/${_id}}`} className="btn btn-primary">
-          Discussion {' '}
-          {comments.length > 0 && (
-            <span className='comment-count'>{comments.length}</span>
-          )}
-        </Link>
-        {!user.loading && userId === user.user._id && (
-          <button  onClick={() => deletePost(_id)} type="button" className="btn btn-danger">
-            <i className="fas fa-times"></i>
-          </button>
-        )}
+        {
+         showActions && (
+           <React.Fragment>
+             <button onClick={() => addLike(_id)} type="button" className="btn btn-light">
+                <i className="fas fa-thumbs-up"></i>{' '}
+                <span>
+                  {likes.length > 0 && (
+                    <span>{likes.length}</span>
+                  )}
+                </span>
+              </button>
+              <button 
+                type="button" 
+                className="btn btn-light"
+                onClick={() => removeLike(_id)}
+                disabled={likes.length === 0 ? 'disabled' : ''}
+              >
+                <i className="fas fa-thumbs-down"></i>
+              </button>
+              <Link to={`/posts/${_id}`} className="btn btn-primary">
+                Discussion {' '}
+                {comments.length > 0 && (
+                  <span className='comment-count'>{comments.length}</span>
+                )}
+              </Link>
+              {!user.loading && userId === user.user._id && (
+                <button  onClick={() => deletePost(_id)} type="button" className="btn btn-danger">
+                  <i className="fas fa-times"></i>
+                </button>
+              )}
+           </React.Fragment>
+         ) 
+        }
       </div>
     </div>
   )
 }
 
 PostItem.propTypes = {
-  user: PropTypes.object.isRequired,
+  user: PropTypes.object,
   post: PropTypes.object.isRequired,
-  addLike: PropTypes.func.isRequired,
-  removeLike: PropTypes.func.isRequired,
-  deletePost: PropTypes.func.isRequired,
+  addLike: PropTypes.func,
+  removeLike: PropTypes.func,
+  deletePost: PropTypes.func,
+  showActions: PropTypes.bool,
 }
