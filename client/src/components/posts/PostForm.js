f2a599172bc4504a03ee1d60c02b103753df10f6
losk773 <losk773@gmail.com>
2020-07-08 03:20:17 +0300

    created PostForm component

---
+++ client/src/components/posts/PostForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const PostForm = ({addPost}) => {
+  const [text, setText] = useState('');
+  const onChange = ({target}) => setText(target.value);
+  const onSubmit = e => {
+    e.preventDefault();
+    addPost({text});
+    setText('');
+  }
+
+  return (
+    <div className="post-form">
+      <div className="bg-primary p">
+        <h3>Say Something...</h3>
+      </div>
+      <form className="form my-1" onSubmit={onSubmit}>
+        <textarea
+          name="text"
+          cols="30"
+          rows="5"
+          placeholder="Create a post"
+          value={text}
+          onChange={onChange}
+          required
+        ></textarea>
+        <input type="submit" className="btn btn-dark my-1" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
+PostForm.propTypes = {
+  addPost: PropTypes.func.isRequired,
+}
