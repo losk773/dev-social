1d13ad124e2e00deb6efb99531da0055157d0530
losk773 <losk773@gmail.com>
2020-07-08 04:45:23 +0300

    implemented comments functionall

--- client/src/components/posts/SinglePost.js
+++ client/src/components/posts/SinglePost.js
@@ -3,8 +3,17 @@ import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 import { Spinner } from '../layout/Spinner';
 import { PostItem } from './PostItem';
+import { CommentForm } from './CommentForm';
+import { CommentItem } from './CommentItem';
 
-const SinglePost = ({ post: { post, loading }, getPostBytId, match: { params } }) => {
+const SinglePost = ({ 
+  user,
+  post: { post, loading }, 
+  getPostBytId, 
+  match: { params },
+  addComment,
+  deleteComment
+}) => {
   useEffect(() => {
     getPostBytId(params.id);
   }, []);
@@ -17,6 +26,24 @@ const SinglePost = ({ post: { post, loading }, getPostBytId, match: { params } }
     <React.Fragment>
       <Link to="/posts" className="btn">Back to Posts</Link>
       <PostItem post={post} showActions={false}/>
+      <CommentForm
+        postId={post._id}
+        addComment={addComment}
+        deleteComment={deleteComment}
+      />
+      <div className="comments">
+        {
+          post.comments.map(comment => (
+            <CommentItem 
+              key={comment._id}
+              user={user}
+              comment={comment}
+              postId={post._id}
+              deleteComment={deleteComment}
+            />
+          ))
+        }
+      </div>
     </React.Fragment>
   )
 }
@@ -24,6 +51,9 @@ const SinglePost = ({ post: { post, loading }, getPostBytId, match: { params } }
 SinglePost.propTypes = {
   match: PropTypes.object.isRequired,
   getPostBytId: PropTypes.func.isRequired,
+  addComment: PropTypes.func,
+  deleteComment: PropTypes.func,
+  user: PropTypes.object.isRequired,
 }
 
 export default withRouter(SinglePost);
--- client/src/containers/SinglePost.js
+++ client/src/containers/SinglePost.js
@@ -2,14 +2,19 @@ import { connect } from 'react-redux';
 import SinglePost from '../components/posts/SinglePost';
 import { 
   getPostBytId,
+  addComment,
+  deleteComment,
 } from '../thunks';
 
-const mapStateToProps = ({post}) => ({
+const mapStateToProps = ({post, user}) => ({
   post,
+  user,
 });
 
 const mapDispatchToProps = dispatch => ({
   getPostBytId: (postId) => dispatch(getPostBytId(postId)),
+  addComment: (postId, formData) => dispatch(addComment(postId, formData)),
+  deleteComment: (postId, commentId) => dispatch(deleteComment(postId, commentId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SinglePost);
